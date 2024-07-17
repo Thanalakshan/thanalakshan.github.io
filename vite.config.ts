17bcb3831bed3c0d165757403350fa026ae606fd
Thanalakshan <bredmendon@gmail.com>
2024-07-17 07:41:17 +0530

    modified:   package-lock.json 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Portfolio/"
+  base: '/Portfolio/',
+  plugins: [react()]
 });
